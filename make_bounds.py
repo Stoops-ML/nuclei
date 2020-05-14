e735c9a938a0b690b1a8b815985b8678a2108eef
danielstoops <danielstoops25@gmail.com>
2020-05-14 23:48:15 +0300

    -updated make_bounds.py with class method

--- make_bounds.py
+++ make_bounds.py
@@ -7,72 +7,80 @@ from pathlib import Path
 import os
 import warnings
 
-root = Tk()
-path = Path(__file__).parent
-out_dir = path / 'coordinates'
-out_dir.mkdir(exist_ok=True)
 
+class ImageCoordinates:
+    def __init__(self, input_folder='images', output_folder='coordinates'):
+        # initialise
+        self.root = Tk()
 
+        # folders
+        self.in_dir = Path(__file__).parent / input_folder
+        self.out_dir = Path(__file__).parent / output_folder
+        self.out_dir.mkdir(exist_ok=True)
 
-def get_coords(file):
-    # setting up a tkinter canvas
-    w = Canvas(root, width=1000, height=1000)
-    w.pack()
+        # get all image files
+        valid_extensions = ['.jpeg', '.jpg', '.png']  # image file extensions
+        self.files = [file for file in os.listdir(self.in_dir)
+                      if re.search(r'[.].*', file).group() in valid_extensions]
 
-    # adding the image
-    image_file = path / 'images' / file
-    open_image = Image.open(image_file)
-    open_image = open_image.resize((1000, 1000))  # resize image
-    img = ImageTk.PhotoImage(open_image)
-    w.create_image(0, 0, image=img, anchor="nw")
+    def get_coordinates(self, file):
+        """print to file user selected coordinates"""
+        # set up tkinter canvas
+        w = Canvas(self.root, width=1000, height=1000)
+        w.pack()
+    
+        # add image to canvas
+        image_file = self.in_dir / file
+        open_image = Image.open(image_file)
+        open_image = open_image.resize((1000, 1000))  # resize image
+        img = ImageTk.PhotoImage(open_image)
+        w.create_image(0, 0, image=img, anchor="nw")
+    
+        # output file for image
+        out_file = self.out_dir / re.sub(r'[.].*', '.txt', str(file))
+        with open(out_file, 'w') as f:  # clear contents of out_file
+            f.write('\n')  # required for printing next new lines later
 
-    # create output file for image
-    out_file = out_dir / re.sub(r'[.].*', '.txt', str(file))
-    if os.path.isfile(out_file):  # delete coordinate file if it exists
-        os.remove(out_file)
-    with open(out_file, 'w') as f:  # create new coordinate file (required to not get error when appending to file)
-        f.write(f"Bounding box coordinates of file {file}\n\n")
-
-    def print_coords(event):
-        # print coordinates to file
-        with open(out_file, 'a') as f:
-            f.write(f"{event.x}, {event.y}\n")
-
-        # count number of lines in file
-        with open(out_file) as f:
-            file_length = len([1 for _ in f])
-
-        # if four coordinates found (i.e. a bounding box) print new line
-        if file_length % 5 == 2:  # four coordinates and \n (5) == title line and \n (2)
+        def print_coordinates(event):
+            # print coordinates to file
             with open(out_file, 'a') as f:
-                f.write('\n')
+                f.write(f"{event.x}, {event.y}\n")
+    
+            # count number of lines in file
+            with open(out_file) as f:
+                file_length = len([1 for _ in f])
+    
+            # if four coordinates found (i.e. a bounding box) print new line
+            if file_length % 5 == 0:  # four coordinates and \n (5)
+                with open(out_file, 'a') as f:
+                    f.write('\n')
+    
+        def close_window():
+            # count number of lines in file
+            with open(out_file) as f:
+                file_length = len([1 for _ in f])
+    
+            # if four coordinates found (i.e. a bounding box) print new line
+            if file_length % 5 != 0:  # four coordinates and one \n line
+                print(f"Last set of coordinates incomplete of file: {file}.\nFix coordinate file manually.\n")
+    
+            # move onto next image
+            w.quit()
+            w.destroy()
+    
+        # buttons
+        w.bind("<Button 1>", print_coordinates)
+        button = Button(self.root, text='Click to quit', command=close_window)
+        w.create_window(35, 35, anchor='nw', window=button)
+    
+        self.root.mainloop()
 
-    def close_window():
-        # count number of lines in file
-        with open(out_file) as f:
-            file_length = len([1 for _ in f])
-
-        # if four coordinates found (i.e. a bounding box) print new line
-        if file_length % 5 != 2:  # four coordinates and one \n line == title line and one \n line
-            print("Last set of coordinates incomplete. Please fix manually\n")
-
-        # move onto next image
-        w.quit()
-        w.destroy()
-
-    # buttons
-    w.bind("<Button 1>", print_coords)
-    button = Button(root, text='Click to quit', command=close_window)
-    w.create_window(35, 35, anchor='nw', window=button)
-
-    root.mainloop()
+    def run_thru_files(self):
+        """run through all images in input directory"""
+        for fimage in self.files:
+            self.get_coordinates(fimage)
 
 
-# files and folders
-valid_extensions = ['.jpeg', '.jpg', '.png']  # image file extensions
-files = [file for file in os.listdir(path / 'images')
-         if re.search(r'[.].*', file).group() in valid_extensions]
-
-# write coordinate files
-for fimage in files:
-    get_coords(fimage)
+if __name__ == '__main__':
+    write_coordinates = ImageCoordinates()
+    write_coordinates.run_thru_files()
