d13b5245a9baa681e79590cc0ca05678293ce2f2
danielstoops <danielstoops25@gmail.com>
2020-05-14 23:25:43 +0300

    -updated make_bounds.py

--- make_bounds.py
+++ make_bounds.py
@@ -13,49 +13,50 @@ out_dir = path / 'coordinates'
 out_dir.mkdir(exist_ok=True)
 
 
+
 def get_coords(file):
     # setting up a tkinter canvas
     w = Canvas(root, width=1000, height=1000)
     w.pack()
 
     # adding the image
-    File = path / 'images' / file
-    original = Image.open(File)
-    original = original.resize((1000, 1000))  # resize image
-    img = ImageTk.PhotoImage(original)
+    image_file = path / 'images' / file
+    open_image = Image.open(image_file)
+    open_image = open_image.resize((1000, 1000))  # resize image
+    img = ImageTk.PhotoImage(open_image)
     w.create_image(0, 0, image=img, anchor="nw")
 
     # create output file for image
-    file_name = re.split(r'[.]', str(file))  # TODO: replace this with re.sub
-    out_file = out_dir / (file_name[0] + '.txt')
-    if os.path.isfile(out_file):
+    out_file = out_dir / re.sub(r'[.].*', '.txt', str(file))
+    if os.path.isfile(out_file):  # delete coordinate file if it exists
         os.remove(out_file)
-    with open(out_file, 'w') as f:  # required as to not get an error with appending to file below on first coordinate
-        f.write(f"{file}\n\n")
+    with open(out_file, 'w') as f:  # create new coordinate file (required to not get error when appending to file)
+        f.write(f"Bounding box coordinates of file {file}\n\n")
 
     def print_coords(event):
-        # print coordinates
+        # print coordinates to file
         with open(out_file, 'a') as f:
             f.write(f"{event.x}, {event.y}\n")
 
-        # number of lines in file
+        # count number of lines in file
         with open(out_file) as f:
             file_length = len([1 for _ in f])
 
-        # if four coordinates found print new line
+        # if four coordinates found (i.e. a bounding box) print new line
         if file_length % 5 == 2:  # four coordinates and \n (5) == title line and \n (2)
             with open(out_file, 'a') as f:
                 f.write('\n')
 
     def close_window():
-        # number of lines in file
+        # count number of lines in file
         with open(out_file) as f:
             file_length = len([1 for _ in f])
 
-        # if four coordinates found print new line
+        # if four coordinates found (i.e. a bounding box) print new line
         if file_length % 5 != 2:  # four coordinates and one \n line == title line and one \n line
-            print("Last set of coordinates incomplete. Please fix manually")  # TODO: make this better. warnings.warn not working for some reason
+            print("Last set of coordinates incomplete. Please fix manually\n")
 
+        # move onto next image
         w.quit()
         w.destroy()
 
@@ -68,6 +69,10 @@ def get_coords(file):
 
 
 # files and folders
-files = [file for file in os.listdir(path / 'images') if not 'DS' in file]  # TODO: change 'if not' to regex with valid file extensions
+valid_extensions = ['.jpeg', '.jpg', '.png']  # image file extensions
+files = [file for file in os.listdir(path / 'images')
+         if re.search(r'[.].*', file).group() in valid_extensions]
+
+# write coordinate files
 for fimage in files:
     get_coords(fimage)
